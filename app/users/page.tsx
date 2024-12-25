5a7ac8cef0a336defcd2fe00bdd61c7793317a87
AhsanKhan <ahsankhan.ubit>
2024-12-25 23:47:28 +0500

    Data Fetching

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,8 +1,32 @@
 import React from 'react'
 
-const UsersPage = () => {
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  website: string;
+}
+const UsersPage = async () => {
+
+  const res = await fetch('https://jsonplaceholder.typicode.com/users', {
+    next: {
+      revalidate: 60
+    }
+  });
+
+  const users: User[] = await res.json();
   return (
+    <>
     <div>Users Page</div>
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>{user.name},EMail : {user.email}</li>
+      ))}
+      <li></li>
+    </ul>
+    </>
   )
 }
 
